98a93d45d535c113fd02d295a6108ce1c519fa49
goqiewd <cheari1018@gmail.com>
2025-11-11 23:12:06 +0900

    x, y 등 스프라이트 위치 관리 딕셔너리 작성
    각 애니메이션 공격, 킥, 방어 등에 따라 관리 예정

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ character = load_image('LuciaSprite.png')
 background.clip_draw(0, 0, background.w, background.h,
                      600, 350, background.w * 1.9, background.h * 1.9)
 
+LuciaSprite={
+    "IDLE":[{"x":0, "y":167, "w":40, "h":230},
+            {"x":40, "y":167, "w":77, "h":230},
+            {"x":77, "y":167, "w":110, "h":230}],
+}
+
 update_canvas()
 
 delay(5)
