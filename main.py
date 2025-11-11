96af0427050441efe28d1bb25402e5a94d5bea97
goqiewd <cheari1018@gmail.com>
2025-11-11 23:38:18 +0900

    while문에서 배경 그릴 것이므로 위의 테스트 줄은 삭제한다.

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ open_canvas(1200,700)
 background = load_image('background.png')
 character = load_image('LuciaSprite.png')
 
-
-background.clip_draw(0, 0, background.w, background.h,
-                     600, 350, background.w * 1.9, background.h * 1.9)
-
 LuciaSprite={
     "IDLE":[{"x":0, "y":167, "w":40, "h":230},
             {"x":40, "y":167, "w":77, "h":230},
