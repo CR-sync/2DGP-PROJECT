e062fd41137d5b4a1f8f49109a010648af9f6393
goqiewd <cheari1018@gmail.com>
2025-11-13 00:09:07 +0900

    스핀킥 설정

--- main.py
+++ main.py
@@ -48,7 +48,21 @@ LuciaSprite={
 
     "Dash_attack" : [{"x":380, "y":537, "w":425, "h":604},
                      {"x":425, "y":537, "w":484, "h":604}],
-    "spinKick" : [],
+    # 스핀 킥은 대시 어택 이후에만 가능
+    "spinKick" : [{"x":0, "y":608, "w":89, "h":670},
+                  {"x":89, "y":608, "w":212, "h":662},
+                  {"x":212, "y":608, "w":303, "h":657},
+                  {"x":303, "y":608, "w":413, "h":657},
+                  {"x":413, "y":608, "w":515, "h":660}],
+
+    "spinKick_combo" : [{"x":0, "y":680, "w":52, "h":745},
+                        {"x":52, "y":682, "w":104, "h":745},
+                        {"x":104, "y":680, "w":178, "h":733},
+                        {"x":178, "y":674, "w":234, "h":730},
+                        {"x":234, "y":666, "w":312, "h":715},
+                        {"x":312, "y":655, "w":398, "h":720},
+                        {"x":398, "y":661, "w":478, "h":720},],
+
 
     "Combo" : [],
 
@@ -79,7 +93,7 @@ def draw_action(action: str, i: int, x: int = 400, y: int = 300, scale: float = 
 
     character.clip_draw(src_x1, src_bottom, src_w, src_h, draw_x, draw_y, dst_w, dst_h)
 
-frame_count = len(LuciaSprite["Dash_attack"])
+frame_count = len(LuciaSprite["spinKick_combo"])
 fps = 8.0
 delay_time = 1.0 / fps
 running = True
@@ -95,7 +109,7 @@ while running:
         HP_bar_down.clip_draw(13, HP_bar_down.h-15, 342-13, 31-15, 600, 550, (342-13)*3 , (31-15)*3 )
         HP_bar_up.clip_draw(x1, HP_bar_up.h-y2, x2-x1, y2-y1, 600, 550, (x2-x1)*3 , (y2-y1)*3 )
 
-        draw_action("Dash_attack", frame, x=130, y=230, scale=7.0)
+        draw_action("spinKick_combo", frame, x=400, y=230, scale=7.0)
 
         update_canvas()
         delay(delay_time)
